2d40b5fb59047b984a82f7db264ccced75d84282
Precious Adedibu <adedibuprecious@gmail.com>
2021-12-11 10:04:14 +0100

    more exceptions

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     credentials: true,
-    origin: 'https://skyewalletapi.herokuapp.com',
+    origin: 'https://skyewallet-frontend.herokuapp.com',
   });
   await app.listen(process.env.PORT || 3002);
 }
